b9fb57234769f5f6f8492882c9549045b3de7e8e
Kevin Johnson <kevin.johnson@viam.com>
2026-01-14 12:02:48 -0500

    revert mainactivity

--- provisioning_app/android/app/src/main/kotlin/com/example/provisioning_app_example/MainActivity.kt
+++ provisioning_app/android/app/src/main/kotlin/com/example/provisioning_app_example/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.example.provisioning_app
 
-import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        // Ensure the intent is set so Flutter/AppAuth can handle the deep link
-        setIntent(intent)
-    }
-}
+class MainActivity : FlutterActivity()
